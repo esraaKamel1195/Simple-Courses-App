431e6a52188b73ecd3ea9bdbc2f4661d2b6112cb
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-07-24 18:01:18 +0200

    Create components and set projects

---
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-course-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-course-dialog.component.html',
+  styleUrl: './edit-course-dialog.component.scss'
+})
+export class EditCourseDialogComponent {
+
+}
