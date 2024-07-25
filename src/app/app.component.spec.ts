91f703c948e3c536fd4ddc4467900a64e0647331
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-07-25 08:02:03 +0200

    Change project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'rxjs-practice' title`, () => {
+  it(`should have the 'Simple-Courses-App' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rxjs-practice');
+    expect(app.title).toEqual('Simple-Courses-App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rxjs-practice');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Simple-Courses-App');
   });
 });
