f4edc4692d9698133cb2bd7624eb28682b9171f8
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-07-26 09:27:26 +0200

    Add Course component content

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatTableModule,
+  ],
   templateUrl: './course.component.html',
-  styleUrl: './course.component.scss'
+  styleUrl: './course.component.scss',
 })
 export class CourseComponent {
+  loading$: Observable<boolean> | undefined;
+  course$: Observable<Course> | undefined;
+  lessons$: Observable<any> | undefined;
 
+  displayedColumns = ["seqNo", "description", "duration"];
+
+  loadLessonsPage(course: Course) {
+    throw new Error('Method not implemented.');
+  }
 }
