726ebfc59c0014c72c20434fe958b007b2eb1c49
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-07-26 10:35:50 +0200

    Upgrade versions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ReactiveFormsModule } from "@angular/forms";
 
 export const appConfig: ApplicationConfig = {
