11b0544ddff2321260df27ff4964e8e02f683aeb
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-07-26 15:14:44 +0200

    Set project cofiguration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { ReactiveFormsModule } from "@angular/forms";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
     provideHttpClient(withInterceptorsFromDi()),
   ],
 };
