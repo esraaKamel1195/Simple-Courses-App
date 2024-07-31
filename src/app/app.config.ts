9adb02c9c5023a6cf27afef5fa1cadb85e2e1ba0
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-07-31 12:27:44 +0200

    Add & install ngrx state management library and module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(),
     provideHttpClient(withInterceptorsFromDi()),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: environment.production }),
   ],
 };
