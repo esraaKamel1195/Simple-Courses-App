6e667e120066972da322feb4f1edfea6bab39115
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-08-05 14:51:01 +0200

    Use createFeatureSelector function

--- src/app/auth/auth.selectors.ts
+++ src/app/auth/auth.selectors.ts
@@ -1,11 +1,13 @@
-import { createSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+export const selectAuthState = createFeatureSelector("auth");
 
 // memorized function
 export const IsLoggedInSelector = createSelector(
-  (state: any) => state['auth'],
-  (auth) => !!auth.user //negiatable condition
+  selectAuthState,
+  (auth: any) => 
+    !!auth.user    //negiatable condition
 );
-
 
 export const IsLoggedOutSelector = createSelector(
   IsLoggedInSelector,
